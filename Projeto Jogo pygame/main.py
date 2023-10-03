effac3c8c01dc168b6183b85564a520694e607bb
ChaosDev. <josecarlossouzapjr@gmail.com>
2023-10-03 20:57:13 -0300

    Testes

--- Projeto Jogo pygame/main.py
+++ Projeto Jogo pygame/main.py
@@ -1,12 +1,15 @@
 import pygame, sys
+from settings import *
+from level import Level
 
 pygame.init()
 Title = "Stick"
-screen = pygame.display.set_mode((1200, 700))
+screen_width, screen_height = 1200, 700
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption(Title)
 clock = pygame.time.Clock()
 text_font = pygame.font.Font(None, 50)
-
+level = Level(level_map,screen)
 
 while True:  # Atualização constante da tela
     for event in pygame.event.get():
@@ -14,5 +17,6 @@ while True:  # Atualização constante da tela
             pygame.quit()
             sys.exit()
 
+    level.run()
     pygame.display.update()
     clock.tick(60)
---
+++ Projeto Jogo pygame/settings.py
@@ -0,0 +1,15 @@
+level_map = ['',
+             '',
+             '',
+             '',
+             '',
+             '',
+             '',
+             '',
+             '',
+             '',
+             ''
+                ]
+
+tile_size = 64
+screen_width, screen_height = 1200, len(level_map) * tile_size
