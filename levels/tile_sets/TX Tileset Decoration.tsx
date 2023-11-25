8783838ceadaf64d71d218471316f76958bdd44c
ChaosDev <143666425+ChaosS-Dev@users.noreply.github.com>
2023-11-25 14:50:22 -0300

    Levels

---
+++ levels/tile_sets/TX Tileset Decoration.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Decorations" tilewidth="32" tileheight="32" tilecount="4096" columns="32" tilerendersize="grid" fillmode="preserve-aspect-fit">
+ <grid orientation="orthogonal" width="36" height="36"/>
+ <image source="Texture/TX Village Props.png" width="1024" height="1024"/>
+</tileset>
