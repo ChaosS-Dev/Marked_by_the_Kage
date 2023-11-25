9e3b05f61a60d2343c61346449ee5afc72ee0e57
ChaosDev <143666425+ChaosS-Dev@users.noreply.github.com>
2023-11-25 14:51:26 -0300

    Delete levels directory

--- levels/tile_sets/TX Tileset Decorations02.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="TX Tileset Decorations02" tilewidth="32" tileheight="32" tilecount="784" columns="28">
- <image source="Texture/TX Tileset Decorations02.png" width="927" height="927"/>
-</tileset>
