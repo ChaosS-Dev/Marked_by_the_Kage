40b6f887aea58727179c9dd65fc7f863f9a519d8
ChaosDev <143666425+ChaosS-Dev@users.noreply.github.com>
2023-11-25 14:52:35 -0300

    Levels
    Tiles e parte gráfica de levels e inimigos que irão ser utilizados

---
+++ Projeto Jogo pygame/levels/tile_sets/TX Tileset Ground - Copia.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TX Tileset Ground - Copia" tilewidth="32" tileheight="32" tilecount="1584" columns="48">
+ <image source="Texture/TX Tileset Ground - Copia.png" width="1550" height="1080"/>
+</tileset>
